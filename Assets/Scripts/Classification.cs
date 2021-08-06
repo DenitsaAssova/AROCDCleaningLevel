27c349cfbbfeb6e179e29117a86a7f9ed7707ed5
Denitsa Asova <deni.assova@gmail.com>
2021-08-06 16:20:58 +0300

    Neural Network for Image Classification implemented

---
+++ Assets/Scripts/Classification.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Barracuda;
+using TMPro;
+using Vuforia;
+
+public class Classification : MonoBehaviour
+{
+    const int IMAGE_SIZE = 224;
+    const string INPUT_NAME = "images";
+    const string OUTPUT_NAME = "Softmax";
+
+  
+    public NNModel onnxFile;
+    public TextAsset labelFile;
+
+    
+    public CameraView cameraView;
+    public Preprocess preprocess;
+   public TextMeshProUGUI uiText;
+
+    private bool bookDetected = false;
+    private bool glassesDetected = false;
+    private bool phoneDetected = false;
+    private bool laptopDetected = false;
+    private bool stopNN = false;
+    private bool scan = true;
+
+    private float timer = 3f;
+    public GameObject continueButton, scanInstructions, continueInstructions, bacFound, scanning;
+    
+    //public TextMeshProUGUI tester, tester2, tester3;
+
+    string[] labels;
+    IWorker worker;
+    private Camera cam;
+    private int frames = 80;
+    private int frames_scanning = 85;
+    void Start()
+    {
+        
+       // VuforiaRuntime.Instance.Deinit();
+       // tester3.text = "Start After vuf deinit";
+        // cam = Camera.main;
+        var model = ModelLoader.Load(onnxFile);
+       // if(model == null) tester3.text = "Start After model null";
+        worker = WorkerFactory.CreateWorker(WorkerFactory.Type.ComputePrecompiled, model);
+        
+        LoadLabels();
+    }
+
+    void LoadLabels()
+    {
+        //get only items in quotes
+        var stringArray = labelFile.text.Split('"').Where((item, index) => index % 2 != 0);
+        //get every other item
+        labels = stringArray.Where((x, i) => i % 2 != 0).ToArray();
+       // tester3.text = labels.Length.ToString();
+    }
+
+    void Update()
+    {
+       // if (cam.GetComponent<VuforiaBehaviour>().enabled == true) uiText.text = "Vuforia is active";
+        WebCamTexture webCamTexture = cameraView.GetCamImage();
+
+       // if (webCamTexture == null) tester.text = "Image is null";
+
+        if (Time.frameCount % frames == 0 && webCamTexture.didUpdateThisFrame && webCamTexture.width > 100 && stopNN==false && scan)
+        {
+            //  tester.text = "if before scale entered";
+            // if (worker == null) tester.text = "worker is null";
+            // uiText.text = "Object is being scanned...";
+            // scanInstructions.GetComponentInChildren<TextMeshProUGUI>().text = "Checking For Bacteria...";
+            //uiText.text ="Entered: " +Time.frameCount.ToString();
+            scanning.SetActive(true);
+           // scanning.GetComponentInChildren<TextMeshProUGUI>().text = "Object scanning...";
+            preprocess.ScaleAndCropImage(webCamTexture, IMAGE_SIZE, RunModel);
+
+           
+            // scanning.SetActive(false);
+        }
+        if (!scan)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                scanning.SetActive(false);
+                timer = 3f;
+                scan = true;
+            }
+        }
+
+
+            if (bookDetected && glassesDetected && laptopDetected && phoneDetected)
+        {
+            scanning.SetActive(false);
+            scanInstructions.SetActive(false);
+            continueInstructions.SetActive(true);
+
+            stopNN = true;
+            continueButton.SetActive(true);
+
+        }
+    }
+
+    void RunModel(byte[] pixels)
+    {
+        //tester2.text = "run model entered";
+        StartCoroutine(RunModelRoutine(pixels));
+    }
+
+    IEnumerator RunModelRoutine(byte[] pixels)
+    {
+        scanInstructions.GetComponentInChildren<TextMeshProUGUI>().text = "Object scanning...";
+        //scanning.GetComponentInChildren<TextMeshProUGUI>().text = "Object scanning...";
+        //scanning.SetActive(true);
+        // bacFound.SetActive(false);
+        //tester2.text = "ienumerator entered";
+        Tensor tensor = TransformInput(pixels);
+       // tester2.text = "transform exited";
+        var inputs = new Dictionary<string, Tensor> {
+            { INPUT_NAME, tensor }
+        };
+       // tester2.text = "dic created";
+        worker.Execute(inputs);
+       // tester2.text = "worker executed";
+        Tensor outputTensor = worker.PeekOutput(OUTPUT_NAME);
+       // tester2.text = "peeked output";
+        // yield return new WaitForCompletion(outputTensor);
+
+        var indexWithHighestProbability = outputTensor.ArgMax()[0];
+       // tester2.text = indexWithHighestProbability.ToString();
+        //get largest output
+        // List<float> temp = outputTensor.ToReadOnlyArray().ToList();
+        // float max = temp.Max();
+        // int index = temp.IndexOf(max);
+
+        //set UI text
+       // uiText.text = labels[indexWithHighestProbability];
+        if (labels[indexWithHighestProbability] == "iPod"
+            || labels[indexWithHighestProbability] == "cellular telephone, cellular phone, cellphone, cell, mobile phone") { phoneDetected = true;
+            
+            scanning.GetComponentInChildren<TextMeshProUGUI>().text = "Bacteria Found!";
+            
+        }
+        else if (labels[indexWithHighestProbability] == "carton" || labels[indexWithHighestProbability] == "bookcase") { bookDetected = true;
+            scanning.GetComponentInChildren<TextMeshProUGUI>().text = "Bacteria Found!";
+            
+        }
+       else if (labels[indexWithHighestProbability] == "laptop, laptop computer"
+            || labels[indexWithHighestProbability] == "notebook, notebook computer") {
+            laptopDetected = true;
+            scanning.GetComponentInChildren<TextMeshProUGUI>().text = "Bacteria Found!";
+           
+        }
+       else if (labels[indexWithHighestProbability] == "sunglasses, dark glasses, shades") {
+            glassesDetected = true;
+            scanning.GetComponentInChildren<TextMeshProUGUI>().text = "Bacteria Found!";
+          
+        } else
+        {
+            scanning.GetComponentInChildren<TextMeshProUGUI>().text = "No Bacteria Found!";
+        
+        }
+
+        scan = false;
+        //dispose tensors
+        tensor.Dispose();
+        outputTensor.Dispose();
+        // scanning.SetActive(false);
+        //uiText.text ="Exited: " +Time.frameCount.ToString();
+        yield return null;
+    }
+
+    //transform from 0-255 to -1 to 1
+    Tensor TransformInput(byte[] pixels)
+    {
+       // tester2.text = "transform entered";
+        float[] transformedPixels = new float[pixels.Length];
+
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            transformedPixels[i] = (pixels[i] - 127f) / 128f;
+            //Debug.Log(transformedPixels[i]);
+        }
+        return new Tensor(1, IMAGE_SIZE, IMAGE_SIZE, 3, transformedPixels);
+    }
+
+    void OnDisable()
+    {
+        worker.Dispose();
+    }
+}
---
+++ Assets/Scripts/Preprocess.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Rendering;
+
+public class Preprocess : MonoBehaviour
+{
+   // public TextMeshProUGUI tester;
+    RenderTexture renderTexture;
+    Vector2 scale = new Vector2(1, 1);
+    Vector2 offset = Vector2.zero;
+   
+    private bool called = false;
+    UnityAction<byte[]> callback;
+   // Texture2D texture;
+    public void ScaleAndCropImage(WebCamTexture webCamTexture, int desiredSize, UnityAction<byte[]> callback)
+    {
+        called = true;
+       // tester.text = "SCale entered";
+        this.callback = callback;
+
+        if (renderTexture == null)
+        {
+            renderTexture = new RenderTexture(desiredSize, desiredSize, 0, RenderTextureFormat.ARGB32);
+        }
+       // tester.text = "after if for render textture";
+        scale.x = (float)webCamTexture.height / (float)webCamTexture.width;
+        offset.x = (1 - scale.x) / 2f;
+        Graphics.Blit(webCamTexture, renderTexture, scale, offset); //crops 
+       // tester.text = "after blit";
+        Texture2D texture; // = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, true);
+        texture = GetRTPixels(renderTexture);
+        texture.Apply();
+        callback.Invoke(texture.GetRawTextureData<byte>().ToArray());
+
+        // texture.Apply();
+        // request = UniversalAsyncGPUReadbackRequest.Request(renderTexture);
+        // OnCompleteReadback(request);
+        //AsyncGPUReadback.Request(renderTexture, 0, TextureFormat.RGB24, OnCompleteReadback);
+    }
+
+    /*private void Update()
+    {
+        if (called == true)
+        {
+            tester.text = "after blitOnCompleteReadbacl entered";
+            if (request.done)
+            {
+                if (request.hasError)
+                {
+                    tester.text = "request has error";
+                    Debug.Log("GPU readback error detected.");
+                    return;
+                }
+                else
+                {
+                    foreach (var item in request.GetData<byte>())
+                    {
+                        Debug.Log(item);
+                    }
+                    callback.Invoke(request.GetData<byte>().ToArray());
+                }
+            }
+           // called = false;
+        }
+    }*/
+   
+    /* void OnCompleteReadback(AsyncGPUReadbackRequest request)
+     {
+         tester.text = "after blitOnCompleteReadbacl entered";
+         if (request.hasError)
+         {
+             tester.text = "request has error";
+             Debug.Log("GPU readback error detected.");
+             return;
+         }
+
+         callback.Invoke(request.GetData<byte>().ToArray());
+     }*/
+
+  
+
+    static public Texture2D GetRTPixels(RenderTexture rt)
+    {
+        // Remember currently active render texture
+        RenderTexture currentActiveRT = RenderTexture.active;
+
+        // Set the supplied RenderTexture as the active one
+        RenderTexture.active = rt;
+        
+        // Create a new Texture2D and read the RenderTexture image into it
+        Texture2D tex = new Texture2D(rt.width, rt.height, TextureFormat.RGB24, rt.mipmapCount > 1);
+        tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+
+        // Restorie previously active render texture
+        RenderTexture.active = currentActiveRT;
+        return tex;
+    }
+}
